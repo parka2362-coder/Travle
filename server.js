53bcd41b7c6d8ec677756fa844eb06d9999440cd
Samuel Park(DSKR-박상언) <parka1251@dstrict.com>
2026-01-19 18:11:48 +0900

    Configure Render API endpoint and CORS

--- server.js
+++ server.js
@@ -36,6 +36,15 @@ function makeKey(city, country) {
 }
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+  next();
+});
 app.use(express.static(__dirname));
 
 app.get('/api/picks', (req, res) => {
